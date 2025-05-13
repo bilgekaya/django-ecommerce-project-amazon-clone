efdff33e4c7468474d6c0103276610083ca8a7e2
bilgekaya <wrx94231@student.wroclaw.merito.pl>
2025-05-13 09:01:47 +0200

    Update views.py

--- DjangoEcommerceApp/views.py
+++ DjangoEcommerceApp/views.py
@@ -30,3 +30,40 @@ def adminLogoutProcess(request):
     logout(request)
     messages.success(request,"Logout Successfully!")
     return HttpResponseRedirect(reverse("admin_login"))
+    from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse
+
+# Create your views here.
+def demoPage(request):
+    return HttpResponse("demo Page")
+
+def demoPageTemplate(request):
+    return render(request, "demo.html")
+
+def adminLogin(request):
+    return render(request, "admin_templates/signin.html")
+
+def adminLoginProcess(request):
+    if request.method == 'POST':  # Form sadece POST isteği ile gönderilirse işleme alınır
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        # Kullanıcı doğrulama
+        user = authenticate(request=request, username=username, password=password)
+        if user is not None:
+            login(request=request, user=user)
+            messages.success(request, "Login successful!")
+            return HttpResponseRedirect(reverse("admin_home"))
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+            return HttpResponseRedirect(reverse("admin_login"))
+    else:
+        return HttpResponseRedirect(reverse("admin_login"))  # Eğer GET ile gelindiyse login sayfasına yönlendir
+
+def adminLogoutProcess(request):
+    logout(request)
+    messages.success(request, "Logout Successfully!")
+    return HttpResponseRedirect(reverse("admin_login"))
